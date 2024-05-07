56356ce9b4a7e87228d95e1732dce82fb3816a2d
Aridunnutemi <152180720+Aridunnutemi@users.noreply.github.com>
2024-05-07 16:21:24 +0100

    Add files via upload

---
+++ classtask/src/App.jsx
@@ -0,0 +1,17 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import classtask from './components'
+import './App.css'
+import classtask from './components/Classtask'
+
+function App() {
+
+
+  return (
+    <>
+    <classtask/>
+    </>
+  )
+}
+
+export default App
---
+++ classtask/src/components/Classtask.jsx
@@ -0,0 +1,13 @@
+
+
+const Classtask= ({ text }) => {
+  const length = text.length;
+
+  return (
+    <div>
+      <p>The length of the text is: {length}</p>
+    </div>
+  );
+};
+
+export default Classtask;
